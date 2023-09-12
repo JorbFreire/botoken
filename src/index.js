2975698743582e92d85786ea03e5de4ccf470916
JorbFreire <jorbfreire@gmail.com>
2023-09-12 15:42:44 -0300

    venon bot hello word

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const venom = require('venom-bot');
+
+const chromiumArgs = [
+  '--disable-web-security', '--no-sandbox', '--disable-web-security',
+  '--aggressive-cache-discard', '--disable-cache', '--disable-application-cache',
+  '--disable-offline-load-stale-cache', '--disk-cache-size=0',
+  '--disable-background-networking', '--disable-default-apps', '--disable-extensions',
+  '--disable-sync', '--disable-translate', '--hide-scrollbars', '--metrics-recording-only',
+  '--mute-audio', '--no-first-run', '--safebrowsing-disable-auto-update',
+  '--ignore-certificate-errors', '--ignore-ssl-errors', '--ignore-certificate-errors-spki-list'
+];
+
+
+venom
+  .create({
+    session: 'session-name', //name of session
+    logQR: false,
+    browserArgs: chromiumArgs,
+  })
+  .then((client) => start(client))
+  .catch((error) => {
+    console.log(error);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Hi' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Welcome Venom 🕷')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((error) => {
+          console.error('Error when sending: ', error); //return object error
+        });
+    }
+  });
+}
+
